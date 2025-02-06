79b91f28f279c6db62a11206c2851a327d1f533b
JC Zale <jczalson@gmail.com>
2025-02-06 16:20:09 +0100

    eureka server

---
+++ src/test/java/com/eai/demo/Login/LoginAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eai.demo.Login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
